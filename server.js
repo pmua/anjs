c529273cf8cb9b2e6df684a643b59d012322896f
pmua <pmua@list.ru>
2017-02-26 15:55:16 +0300

    Prepare for express

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
-console.log('This is Node.js running on Android.');
-console.log('New edit');
+// DEPENDENCIES AND SETUP
+// ===============================================
+
+var express = require('express'),
+  app = express(),
+  port = Number(process.env.PORT || 8080);
+
+// ROUTES
+// ===============================================
+
+// Define the home page route.
+app.get('/', function(req, res) {
+  res.send('Our first route is working.:)');
+});
+
+// START THE SERVER
+// ===============================================
+
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
